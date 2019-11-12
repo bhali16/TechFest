e952f4bdc077eb7e27e98f283754a5755c7607d4
Afnan sohail <SohailKhan12336>
2019-11-12 13:24:36 +0500

    my new source code

---
+++ forms.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>The web development competition</title>
+	<link rel="stylesheet" type="text/css" href="event.css">
+</head>
+<body>
+	
+	<h2>The  Tech Fest </h2>
+
+	<ul class="first_ul">
+		<li class="active"><a class="active" href="event.html"  target="_blank"> Home</a></li>
+		<li><a href="#"  target="_blank">Register</a></li>
+	</ul>
+
+
+
+
+
+	<div class="wrapper">
+		<div>
+
+<div class="container">
+
+
+<form action ="techfest.php" method="post">
+
+<legend><h3 class="pacific"> SIGN UP !!!!</h3></legend>
+<fieldset>
+<h3 class="family">Name:<br  />
+<input type="text"  name="name" maxlength="20" placeholder="please enter your name" required="required" /></h3>
+<h3 class="valid">Email:<br  />
+<input type="text"  name="email" maxlength="20" placeholder="please enter your email" required="required" /></h3>
+<h3 class="valid">Category:<br  />
+<input type="text" name="category" maxlength="20" placeholder="please enter your password" required="required" /></h3>
+
+<input type="submit"  name="submit" value="SIGN UP" href="13 REASONS WHY.html" class="btn btn-success" ></a></button>
+
+
+</fieldset>
+</form>
+<a href="Hii.html"><u class="white">ForgetPassword?</u></a>
+</div>
+		
+
+		</div>
+	</div>
+
+
+<div class="events">
+	
+	<img src="android.jpg" style="width: 320px">
+	<img src="pic2.jpg"  style="height: 320px">
+	<img src="p.jpeg" style="height: 320px">
+	<img src="modern.jpg" style="height: 320px">
+
+
+
+</div>
+
---
+++ techfest.php
@@ -0,0 +1,63 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>The web development competition</title>
+    <link rel="stylesheet" type="text/css" href="event.css">
+</head>
+<body>
+    
+    <h2>The  Tech Fest </h2>
+
+    <ul class="first_ul">
+        <li class="active"><a class="active" href="event.html"  target="_blank">Home</a></li>
+        
+    </ul>
+
+
+
+
+
+    <div class="wrappers">
+        <div id="div1">
+          <h1 style="color: white "  style="margin-left: 40px;" >YOU  HAVE SUCESSFULLY SIGNED UP!!!</h1>
+
+        </div>
+    </div>
+
+</body>
+</html>
+ <?php 
+if(isset($_POST['submit']))
+{
+  
+ $username= $_POST['name'];
+$email= $_POST['email'];
+$category=$_POST['category'];
+    
+
+    
+    $connection=mysqli_connect('localhost','root','root','techfest');
+    if($connection){
+    
+        echo "";
+    }
+    else
+    {
+        
+        echo "";
+    }
+    
+  
+    
+    $query="INSERT INTO tech_fest_table(name,email,category)  VALUES ('$username','$email','$category')";
+    $result=mysqli_query($connection,$query);
+ 
+    
+}
+
+
+
+
+
+?>
